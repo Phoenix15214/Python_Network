6308c33cd99ecff23344a58072dbf21e5ea493b9
Phoenix15214 <2198361886@qq.com>
2026-03-04 17:26:22 +0800

    新增TC传输实验程序

--- test/Transmission.py
+++ test/Transmission.py
@@ -1,4 +1,81 @@
 import socket
 import cv2
-from threading import Thread
+from multiprocessing import Process, Pipe
+
+send_pipe1, recv_pipe1 = Pipe()
+send_pipe2, recv_pipe2 = Pipe()
+
+def Video_Process(tx, rx):
+    cap = cv2.VideoCapture(0)
+    isConnected = False
+    if not cap.isOpened():
+        print("无法打开摄像头")
+        exit()
+    while True:
+        if rx.poll():
+            isConnected = rx.recv()
+        ret, frame = cap.read()
+        if not ret:
+            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        contours = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        total_area = 0.0
+        cx, cy = 0.0, 0.0
+        if len(contours) > 0:
+            # 提取最大面积轮廓
+            largest_cnt = max(contours, key=cv2.contourArea)
+            # 使用cv2.moments计算图形矩
+            m = cv2.moments(largest_cnt)
+            # 计算中心坐标
+            # 防止除0
+            if m['m00'] > 0:
+                cx = int(m['m10'] / m['m00'])
+                cy = int(m['m01'] / m['m00'])
+                # 绘制中心点
+                cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
+                cv2.putText(frame, f'Avg X: {cx}', (10, 70),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+                cv2.putText(frame, f'contour: {len(contours)}', (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+                for cnt in contours:
+                    cv2.drawContours(frame, cnt, -1, (0, 255, 0), 3)
+        cv2.imshow("image", frame)
+        cv2.waitKey(1)
+        send_msg = f"{cx}, {cy}\n"
+        if isConnected:
+            print(send_msg)
+            tx.send(send_msg)
+
+
+def Send_Process(tx, rx):
+    while True:
+        isConnected = False
+        connect_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connect_socket.bind(("", 11451))
+        connect_socket.listen(3)
+        server_socket, client_addr = connect_socket.accept()
+        isConnected = True
+        tx.send(isConnected)
+        while True:
+            msg = rx.recv()
+            try:
+                server_socket.send(msg.encode("utf8"))
+            except:
+                print("客户端断开连接")
+                isConnected = False
+                tx.send(isConnected)
+                break
+
+if __name__ == "__main__":
+    p1 = Process(target=Video_Process, args=(send_pipe1, recv_pipe2))
+    p2 = Process(target=Send_Process, args=(send_pipe2, recv_pipe1))
+
+    p1.start()
+    p2.start()
+
+
+
+
+
 
--- test/test.py
+++ test/test.py
@@ -1,22 +1,13 @@
-from threading import Thread
-from multiprocessing import Process
-import time
+from multiprocessing import Pipe
 
-n = 500000000
+parent_conn, child_conn = Pipe()
 
-def add(n: int):
-    sum = 0
-    while sum < n:
-        sum+=1
-    print(f"当前线程累加了{sum}次")
+# 子进程发送数据（此处模拟）
+child_conn.send("hello")
 
-if __name__ == "__main__":
-    t1 = Process(target=add, args=(n/2, ))
-    t2 = Process(target=add, args=(n/2, ))
-    t1.start()
-    t2.start()
-    start = time.time()
-    t1.join()
-    t2.join()
-    end = time.time()
-    print(f"处理耗时{end - start}秒")
+# 检查是否有数据
+if parent_conn.poll():
+    msg = parent_conn.recv()
+    print(msg)  # 输出 "hello"
+else:
+    print("No data")
